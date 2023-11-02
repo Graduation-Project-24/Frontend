027f94a0cfbdb45025df9d376ce49dc19c5f5b76
Omar El-Nahas <omarelnahas1141@gmail.com>
2023-11-02 18:51:35 +0200

    up

---
+++ my-app/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "./index.css";
+
+function App() {
+  return (
+    <>  
+      <h1 className="text-center p-5">Graduation Project</h1>
+    </>
+  );
+}
+
+export default App;
