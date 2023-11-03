8a166c5dc4a041c69a116169f147ade69720facf
Ahmedaboelmaged <aaboel53@gmail.com>
2023-11-03 14:28:25 +0200

    abo

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./index.css";
+import Abo from "./aboEsmael/abo";
 
 function App() {
   return (
     <>  
       <h1 className="text-center p-5">Graduation Project</h1>
       test abomagad
+      <Abo />
     </>
   );
 }
---
+++ my-app/src/aboEsmael/abo.jsx
@@ -0,0 +1,12 @@
+import React from "react-dom"
+
+
+function Abo() {
+    return (
+      <>  
+        <p>404</p>
+      </>
+    );
+  }
+  
+  export default Abo;
