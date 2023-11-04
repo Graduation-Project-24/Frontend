1b1704ba1d46f6dbd074074e1ca977ecc5059c9b
Omar El-Nahas <omarelnahas1141@gmail.com>
2023-11-05 01:19:35 +0200

    Navigation

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,16 +1,11 @@
-import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./index.css";
-import Abo from "./aboEsmael/abo";
+import Nav from "./component/Navigation/Nav";
 
 function App() {
   return (
     <>  
-      <h1 className="text-center p-5">Graduation Project</h1>abomagad
-      test abomagad
-      <Abo />
-
-      test esmmm
+      <Nav />
     </>
   );
 }
